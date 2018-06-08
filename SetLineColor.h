39c39a0e20e9f8727cf2cf6692e3e492a158b8ba
Layika <mwoleksy@gmail.com>
2018-06-08 16:54:01 +0200

    Added set line color

--- SetLineColor.h
+++ SetLineColor.h
@@ -6,12 +6,22 @@
 class SetLineColor : public BaseCommand {
 	public:
 		// Initializing set_line_color command
-		SetLineColor(MyFrame* app) : BaseCommand("set_line_color", 1, app) {}
+		SetLineColor(MyFrame* app) : BaseCommand("set_line_color", 3, app) {}
 
 		// Overridden Execute method, sets pen color
-		bool Execute(std::vector<std::string> args) { return true;  }
+		bool Execute(std::vector<std::string> args) {
+			// Get r g b numbers
+			std::vector<int> rgb(3);
+			rgb[0] = atoi(args[1].c_str());
+			rgb[1] = atoi(args[2].c_str());
+			rgb[2] = atoi(args[3].c_str());
+
+			app->drawingColor.Set(rgb[0], rgb[1], rgb[2]);
+
+			return true;
+		}
 
 		// Overridden documentation methods
-		std::string Args() const { return "color"; }
+		std::string Args() const { return "r g b"; }
 		std::string Help() const { return "Sets pen color"; }
 };

04fa58b339dd05bd2fec66f9f683045f97950c62
Werseter <werseter@interia.pl>
2018-06-08 16:46:16 +0200

    Added Line.h

--- Commands/BaseCommand.cpp
+++
@@ -1,1 +0,0 @@
-#include "BaseCommand.h"
--- Line.h
+++ Line.h
@@ -11,12 +11,16 @@ class Line : public BaseObject {
 
 	public:
 		// Line is constructed with basic properties
-		Line(Data3D start, Data3D end) : BaseObject("line"), start(start), end(end) {}
+		Line(Data3D start, Data3D end) : BaseObject("line"), start(start), end(end) { GeneratePoints(); }
 
 		// Each object has its own representation, this method supplements main representation printing method
 		std::string Repr() const { return BaseObject::Repr() + GetCoordinatesString(start) + " " + GetCoordinatesString(end); }
 
-		void GeneratePoints() {}
+		void GeneratePoints() {
+			Data3D temp(6);
+			temp = { start[0], start[1], start[2], end[0], end[1], end[2] };
+			points.push_back(temp);
+		}
 
 		void MoveOrigins(Data3D vec) {
 			for (int i = 0; i < 3; ++i) {

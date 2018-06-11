d986adb53483becfaa990ea987ebbaf2af51391a
Werseter <werseter@interia.pl>
2018-06-11 21:30:45 +0200

    Added Sphere rendering

--- Sphere.h
+++ Sphere.h
@@ -14,12 +14,29 @@ class Sphere : public BaseObject {
 	public:
 		Sphere() : BaseObject("sphere") {}
 		// Sphere is constructed with basic properties
-		Sphere(Data3D pos, float radius, unsigned int meridians, unsigned int parallels) : BaseObject("sphere"), radius(radius), pos(pos), meridians(meridians), parallels(parallels) {}
+		Sphere(Data3D pos, float radius, unsigned int meridians, unsigned int parallels) : BaseObject("sphere"), radius(radius), pos(pos), meridians(meridians), parallels(parallels) { GeneratePoints(); }
 
 		// Each object has its own representation, this method supplements main representation printing method
 		std::string Repr() const { return BaseObject::Repr() + GetCoordinatesString(pos) + " " + toString(radius); }
 
-		void GeneratePoints() {}
+		void GeneratePoints() {
+			const double PI = acos(-1);
+			double diff_1 = PI / meridians;
+			double diff_2 = PI / parallels;
+
+			for (double phi = 0.0; phi < 2 * PI; phi += PI / meridians) {
+				for (double theta = 0.0; theta < PI; theta += PI / parallels) {
+					Data3D temp(6);
+					temp[0] = radius * cos(phi) * sin(theta) + pos[0];
+					temp[1] = radius * sin(phi) * sin(theta) + pos[1];
+					temp[2] = radius * cos(theta) + pos[2];
+					temp[3] = radius * cos(phi + diff_1) * sin(theta + diff_2) + pos[0];
+					temp[4] = radius * sin(phi + diff_1) * sin(theta + diff_2) + pos[1];
+					temp[5] = radius * cos(theta + diff_2) + pos[2];
+					points.push_back(temp);
+				}
+			}
+		}
 
 		void MoveOrigins(Data3D vec) {
 			for (int i = 0; i < 3; ++i) {
